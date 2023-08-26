b4a8e8f01bb6450d5747d9d601e2614e5ffc0b70
lorow <smykupyka@gmail.com>
2023-08-27 00:42:40 +0200

    Animate the loading screen

--- src/portfolio/sections/LoadingPage.tsx
+++ src/portfolio/sections/LoadingPage.tsx
@@ -1,7 +1,32 @@
+import { createEffect, createSignal } from "solid-js"
+import "../components/styles/loadingPage.scss"
+
 export default function LoadingPage(props: any) {
- return (   
-    <section classList={{visible: props.progress() !== 100, invisible: props.progress() === 100}} class=" bg-black fixed w-full h-screen z-[999]">
-        <span class="text-white text-5xl">LOADING: {props.progress()}</span>
-    </section>
- )
+    const [shouldFadeout, setShouldFadeOut] = createSignal(false);
+        
+    createEffect(() => {
+        if (props.progress() === 100) {
+            setTimeout(()=>{setShouldFadeOut(true)}, 200);    
+        }
+    });
+
+    return (   
+        <section 
+            classList={{"opacity-100": !shouldFadeout(), "opacity-0": shouldFadeout(), visible: !shouldFadeout(), invisible: shouldFadeout() }}
+            class="bg-black fixed w-full h-screen z-[999] ease-in-out duration-500"
+        >
+            <div class="center">
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+                <div class="wave"></div>
+            </div>
+        </section>
+    )
 }
