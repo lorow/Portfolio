a5f14f40c5dc84cacb4ff3d7b813f8d500843bca
lorow <smykupyka@gmail.com>
2023-08-26 13:55:18 +0200

    Improve scrollPointer design

--- src/portfolio/components/ScrollPointer.tsx
+++ src/portfolio/components/ScrollPointer.tsx
@@ -2,6 +2,6 @@ import "./styles/scrollPointer.scss"
 
 export default function ScrollPointer(props: any) {
         return (
-        <div class={(props.currentSection() === props.maxSection ? "fade-out"  : "fade-in") + " fixed left-1/2 bottom-[4rem] scroll"}></div>
+        <div class={(props.currentSection() === props.maxSection ? "fade-out"  : "fade-in") + " fixed left-1/2 bottom-[4.5rem] scroll"}></div>
     )
 }
