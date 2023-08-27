23dffac075537eed9ddaff27952d9bda6114fed9
lorow <smykupyka@gmail.com>
2023-08-28 00:29:58 +0200

    Initial about me content

--- src/portfolio/sections/AboutSection.tsx
+++ src/portfolio/sections/AboutSection.tsx
@@ -4,7 +4,24 @@ export default function AboutSection(){
             <div class="flex text-white pl-20 pt-10 w-full pointer-events-auto">
                 <div class="flex flex-col max-lg ml-5">
                     <h1 class="text-7xl antialiased leading-[10rem] h-32">About</h1>
+                    <p class="text-2xl">To tell you the truth, I'm no magician.</p>
+                    <p class="text-2xl">But I do make stuff work!</p>
                 </div>
+            </div>
+
+            <div class="text-white flex flex-col">
+                <span>Alright, now that we've got all of this out of the way, let me tell you something about me.</span>
+
+                <span>I'm a Python backend developer with 3 years of "commercial experience" on my back.
+                I love working on side projects in my free time, they are "excuses" for me to get out of my comfort zone and try something new. 
+                Be it a new language, new tech, a completely different field, I will learn something.</span>
+
+                <span>But it's not just work that makes us tick.</span>
+                <span>I also love riding around the city on my trusty EUC, listenting to the harder styles of music and exploring and taking pictures of beautiful worlds inside of VRChat.</span>
+                <span>I find it quite amazing to be able to explore something shy of pure creators imagination, to see and experience what they imaigned in so many more ways is, I think, beautiful.</span>
+                <span>I don't have them posted anywhere yet tho.</span>
+
+                <span>Anyway, sorry for the rambling, and thanks for visiting!.</span>   
             </div>
         </section>
     )
