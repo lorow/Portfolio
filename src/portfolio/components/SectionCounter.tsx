09365b0ab83e26d2bd8c1d95d73084916f1695a6
lorow <smykupyka@gmail.com>
2023-08-30 00:27:01 +0200

    Make the section counter scale
    TODO: add updating sections only when the section is in view, disable fancy scroll on mobile

--- src/portfolio/components/SectionCounter.tsx
+++ src/portfolio/components/SectionCounter.tsx
@@ -2,7 +2,7 @@
 import './styles/counter.scss'
 
 export default function SectionCounter(props: any) {
-    const baseNumberStyle = "m-0 p-0 text-7xl absolute active"
+    const baseNumberStyle = "m-0 p-0 text-3xl sm:text-7xl absolute active"
 
     const isActive = (sectionToCheck: number) => ({
         active: props.currentSection() === sectionToCheck,
@@ -10,8 +10,8 @@ export default function SectionCounter(props: any) {
     })
 
     return (
-        <div class="fixed flex-row left-20 bottom-10 text-white overflow-y-hidden w-[10rem]" id="page-counter">
-            <span class="m-0 p-0 text-7xl">/0</span>
+        <div class="fixed flex-row left-5 sm:left-20 bottom-5 sm:bottom-10 text-white overflow-y-hidden w-[4rem] sm:w-[10rem]" id="page-counter">
+            <span class="m-0 p-0 text-3xl sm:text-7xl">/0</span>
             <div class="inline-block m-0 p-0">
                 <div class="overflow-hidden flex flex-col">
                     <span classList={isActive(1)} class={baseNumberStyle}>1</span>
