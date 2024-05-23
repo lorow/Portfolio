5af16997dd5dd67ab258d856ba1d85e887326af6
lorow <smykupyka@gmail.com>
2024-05-24 00:28:16 +0200

    refactor menu to be a bit more reliable

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,93 +1,106 @@
+import { createEffect, createSignal, onMount } from 'solid-js';
+import { animate, stagger } from 'motion';
 import Socials from './socials';
 import './styles/menu.scss'
 
-export default function Menu(props: any) {
-    let setSection = props.setSection;
-    let menuToggle!: HTMLInputElement;
-    const HandleRouteSwitch = (route: number) => {
-        menuToggle.checked = false;
-        setSection(route);
-    }
+interface MenuItemProps {
+  registerSelf: (ref: HTMLElement) => void,
+  HandleRouteSwitch: () => void,
+  routeName: string,
+  subRouteName: string
+  reapeatSubRouteNameTimes: number,
+}
 
-    return (
-        <>
-        <input ref={menuToggle} type="checkbox" id="burger-toggle" />
-        <label for="burger-toggle" class="burger-menu">
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div> 
-        </label>
-    
-        <div class="menu flex flex-col gap-6">
-        <div class="menu-item">
-                <a class="menu-item-link" href="#home" onClick={() => HandleRouteSwitch(1)}>/Home</a>
-                <div class="marquee">
-                    <div class="marquee-inner" aria-hidden="true">
-                        <span>Home</span>
-                        <span>Home</span>
-                        <span>Home</span>
-                        <span>Home</span>
-                        <span>Home</span>
-                        <span>Home</span>
-                        <span>Home</span>
-                        <span>Home</span>
-                        <span>Home</span>
-                        <span>Home</span>
-                        <span>Home</span>
-                        <span>Home</span>
-                    </div>
-                </div>
-            </div>
-            <div class="menu-item">
-                <a class="menu-item-link" href="#projects" onClick={() => HandleRouteSwitch(2)}>/Projects</a>
-                <div class="marquee">
-                    <div class="marquee-inner" aria-hidden="true">
-                    <span>Projects</span>
-                        <span>Works</span>
-                        <span>Works</span>
-                        <span>Works</span>
-                        <span>Works</span>
-                        <span>Works</span>
-                        <span>Works</span>
-                        <span>Works</span>
-                    </div>
-                </div>
-            </div>
-            <div class="menu-item">
-                <a class="menu-item-link" href="#blog" onClick={() => HandleRouteSwitch(3)}>/Blog</a>
-                <div class="marquee">
-                    <div class="marquee-inner" aria-hidden="true">
-                        <span>Blog</span>
-                        <span>Blog</span>
-                        <span>Blog</span>
-                        <span>Blog</span>
-                        <span>Blog</span>
-                        <span>Blog</span>
-                        <span>Blog</span>
-                        <span>Blog</span>
-                        <span>Blog</span>
-                        <span>Blog</span>
-                        <span>Blog</span>
-                    </div>
-                </div>
-            </div>
-            <div class="menu-item">
-                <a class="menu-item-link" href="#about" onClick={() => HandleRouteSwitch(4)}>/About</a>
-                <div class="marquee">
-                    <div class="marquee-inner" aria-hidden="true">
-                        <span>About</span>
-                        <span>About</span>
-                        <span>About</span>
-                        <span>About</span>
-                        <span>About</span>
-                        <span>About</span>
-                        <span>About</span>
-                        <span>About</span>
-                    </div>
-                </div>
-            </div>
-            <Socials shouldHideOnMobile={false}/>
+function MenuItem(props: MenuItemProps) {
+  let itemRef!: HTMLDivElement;
+  onMount(() => props.registerSelf(itemRef));
+
+  return (
+    <div ref={itemRef} class="menu-item">
+      <a class="menu-item-link" href="#home" onClick={() => props.HandleRouteSwitch()}>/{props.routeName}</a>
+      <div class="marquee">
+        <div class="marquee-inner" aria-hidden="true">
+          {Array.from(Array(props.reapeatSubRouteNameTimes)).map((_) => <span>{props.subRouteName}</span>)}
         </div>
-        </>
-    )
+      </div>
+    </div>
+  )
+}
+
+export default function Menu(props: any) {
+  const [menuVisible, toggleMenuVisible] = createSignal(false);
+
+  const [registeredItems, setRegisteredItems] = createSignal<HTMLElement[]>([]);
+  const addRef = (ref: HTMLElement) => setRegisteredItems([...registeredItems(), ref]);
+
+  let setSection = props.setSection;
+  let menuToggle!: HTMLInputElement;
+  const HandleRouteSwitch = (route: number) => {
+    setSection(route);
+  }
+
+  createEffect(() => {
+    if (registeredItems().length === 0)
+      return;
+
+    if (menuVisible()) {
+      animate(registeredItems(),
+        { opacity: 1 },
+        { delay: stagger(0.3) }
+      );
+    }
+    else {
+      animate(registeredItems(),
+        { opacity: 0 },
+        { delay: 0 }
+      );
+    }
+  });
+
+  return (
+    <>
+      <input ref={menuToggle} onChange={() => toggleMenuVisible(!menuVisible())} type="checkbox" id="burger-toggle" />
+      <label for="burger-toggle" class="burger-menu">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+      </label>
+
+      <div id="menu-list" class="menu flex flex-col gap-6">
+        <MenuItem
+          registerSelf={addRef}
+          HandleRouteSwitch={() => HandleRouteSwitch(1)}
+          routeName="Home"
+          subRouteName="Home"
+          reapeatSubRouteNameTimes={10}
+        />
+
+        <MenuItem
+          registerSelf={addRef}
+          HandleRouteSwitch={() => HandleRouteSwitch(2)}
+          routeName="Projects"
+          subRouteName="Works"
+          reapeatSubRouteNameTimes={10}
+        />
+
+        <MenuItem
+          registerSelf={addRef}
+          HandleRouteSwitch={() => HandleRouteSwitch(3)}
+          routeName="Blog"
+          subRouteName="Blog"
+          reapeatSubRouteNameTimes={10}
+        />
+
+        <MenuItem
+          registerSelf={addRef}
+          HandleRouteSwitch={() => HandleRouteSwitch(4)}
+          routeName="About"
+          subRouteName="About"
+          reapeatSubRouteNameTimes={10}
+        />
+
+        <Socials shouldHideOnMobile={false} />
+      </div>
+    </>
+  )
 }
