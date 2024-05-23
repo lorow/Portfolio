ab271f21b8d5ac961c4c8912f0133a70510844be
lorow <smykupyka@gmail.com>
2024-05-24 00:29:18 +0200

    adjust timing a bit

--- src/components/typrewritter.tsx
+++ src/components/typrewritter.tsx
@@ -22,7 +22,7 @@ enum TypeWriterState {
 
 
 // inspired by https://ui.aceternity.com/components/typewriter-effect
-export default function Typewritter(props: TypewritterProps){
+export default function Typewritter(props: TypewritterProps) {
   const [currentWordIndex, setCurrentWordIndex] = createSignal(0);
   const [currentCharIndex, setcurrentCharIndex] = createSignal(0);
   const [writingState, transitionWritingState] = createSignal(TypeWriterState.INITIAL_DEYLAY);
@@ -32,56 +32,56 @@ export default function Typewritter(props: TypewritterProps){
     return word.split("")
   })
 
-  createEffect(()=>{
-      if (props.currentSection == props.typeWriterSection) {}
+  createEffect(() => {
+    if (props.currentSection == props.typeWriterSection) { }
 
-      switch(writingState()){
-        case TypeWriterState.INITIAL_DEYLAY:
+    switch (writingState()) {
+      case TypeWriterState.INITIAL_DEYLAY:
+        setTimeout(() => {
+          transitionWritingState(TypeWriterState.TYPE_WORD);
+        }, 1500);
+        break;
+      case TypeWriterState.TYPE_WORD:
+        if (currentCharIndex() < wordsArray[currentWordIndex()].length)
           setTimeout(() => {
-            transitionWritingState(TypeWriterState.TYPE_WORD);
-          }, 700);
-          break; 
-        case TypeWriterState.TYPE_WORD:
-            if (currentCharIndex() < wordsArray[currentWordIndex()].length)
-              setTimeout(()=> {
-                setcurrentCharIndex(currentCharIndex() + 1);
-              }, typingSpeed);
-            else
-              transitionWritingState(TypeWriterState.WAIT_DELAY);
-            break;
-          case TypeWriterState.WAIT_DELAY:
-            setTimeout(() => {
-              transitionWritingState(TypeWriterState.ERASE_WORD);
-            }, 4000)
-          break;
-          case TypeWriterState.ERASE_WORD:
-            if (currentCharIndex() > 0)
-              setTimeout(()=> {
-                setcurrentCharIndex(currentCharIndex() - 1);
-              }, typingSpeed);
-            else
-              transitionWritingState(TypeWriterState.WAIT_AFTER_DELETE);
-          break;
-          case TypeWriterState.WAIT_AFTER_DELETE:
-            setTimeout(() => {
-              transitionWritingState(TypeWriterState.CHANGE_WORD);
-            }, 1200)
-            break;
-          case TypeWriterState.CHANGE_WORD:
-            if (currentWordIndex() < wordsArray.length - 1)
-              setCurrentWordIndex(currentWordIndex() + 1);
-            else
-              setCurrentWordIndex(0);
-            transitionWritingState(TypeWriterState.TYPE_WORD);
-          break;
-      }
-    });
+            setcurrentCharIndex(currentCharIndex() + 1);
+          }, typingSpeed);
+        else
+          transitionWritingState(TypeWriterState.WAIT_DELAY);
+        break;
+      case TypeWriterState.WAIT_DELAY:
+        setTimeout(() => {
+          transitionWritingState(TypeWriterState.ERASE_WORD);
+        }, 4000)
+        break;
+      case TypeWriterState.ERASE_WORD:
+        if (currentCharIndex() > 0)
+          setTimeout(() => {
+            setcurrentCharIndex(currentCharIndex() - 1);
+          }, typingSpeed);
+        else
+          transitionWritingState(TypeWriterState.WAIT_AFTER_DELETE);
+        break;
+      case TypeWriterState.WAIT_AFTER_DELETE:
+        setTimeout(() => {
+          transitionWritingState(TypeWriterState.CHANGE_WORD);
+        }, 1200)
+        break;
+      case TypeWriterState.CHANGE_WORD:
+        if (currentWordIndex() < wordsArray.length - 1)
+          setCurrentWordIndex(currentWordIndex() + 1);
+        else
+          setCurrentWordIndex(0);
+        transitionWritingState(TypeWriterState.TYPE_WORD);
+        break;
+    }
+  });
 
   return (
     <div class={cn(props.className)}>
       <div class="inline">
-          <span>{wordsArray[currentWordIndex()].slice(0, currentCharIndex())}</span>
-        </div>
+        <span>{wordsArray[currentWordIndex()].slice(0, currentCharIndex())}</span>
+      </div>
       <div class="cursor">|</div>
     </div>
   )
--- src/sections/LoadingPage.tsx
+++ src/sections/LoadingPage.tsx
@@ -6,14 +6,14 @@ export default function LoadingPage(props: any) {
 
   createEffect(() => {
     if (props.progress() === 100) {
-      setTimeout(() => { setShouldFadeOut(true) }, 200);
+      setTimeout(() => { setShouldFadeOut(true) }, 300);
     }
   });
 
   return (
     <section
       classList={{ "opacity-100": !shouldFadeout(), "opacity-0": shouldFadeout(), visible: !shouldFadeout(), invisible: shouldFadeout() }}
-      class="bg-black fixed w-full h-screen z-[999] ease-in-out duration-500"
+      class="bg-black fixed w-full h-screen z-[999] ease-in-out duration-1000"
     >
       <div class="center">
         <div class="wave"></div>
