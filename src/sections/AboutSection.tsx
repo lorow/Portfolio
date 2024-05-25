be455edba12ec7f2dc2e06af26efcff289a1563d
lorow <smykupyka@gmail.com>
2024-05-26 00:37:51 +0200

    Switch everything to lorow

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -13,13 +13,13 @@ export default function AboutSection() {
 
         <div class="text-white flex flex-col gap-10 md:text-lg pt-5 xl:mt-10 pl-7 md:ml-20 pr-2 lg:w-[50vw] pointer-events-auto">
           <div class="text-white flex flex-col gap-2 md:text-lg">
-            <span>Hey there! I'm Zdzisław, a passionate backend developer who's always tinkering with something. Be it new language, new framework, new tech.</span>
+            <span>Hey there! I'm Lorow, a passionate backend developer who's always tinkering with something. Be it new language, new framework, new tech.</span>
             <span>There's always something new to learn, and that keeps me hooked, so if I find it interesting I will find a way to try it out</span>
             <span>Right now, I'm learning more about VR/AR and ML, working on cool stuff like ETVR.</span>
           </div>
 
           <div class="text-white flex flex-col gap-2 md:text-lg">
-            <span>But it's not just work that makes us tick</span>
+            <span>But it's not just work that makes us tick.</span>
             <span>I've recently picked up a hobby of taking pictures of the worlds I explore in VR in VRChat, though I don't have them posted anywhere yet.</span>
             <span>I find it quite amazing to be able to explore something shy of pure creators' imagination. To see and experience what they imagined in so many more ways.</span>
           </div>
