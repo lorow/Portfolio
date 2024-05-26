09d26333477a83db20de4626ddb5eb194dd83e73
lorow <smykupyka@gmail.com>
2024-05-26 14:40:26 +0200

    switch div to footer

--- src/sections/FooterSection.tsx
+++ src/sections/FooterSection.tsx
@@ -1,7 +1,7 @@
 export default function FooterSection() {
   return (
-    <div class="pb-1 w-full, min-h-[4em] flex flex-row justify-center items-center text-white opacity-10">
+    <footer class="pb-1 w-full, min-h-[4em] flex flex-row justify-center items-center text-white opacity-10">
       <p>Made with SolidJS, Threejs, TailwindCSS and &lt;3</p>
-    </div>
+    </footer>
   )
 }
